f46aa32a46f2425e45fe52ded6d5d83e7b1de99d
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 13:06:15 -0700

    add spacers

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,13 @@ export default function Navbar() {
         <Flex direction="row" w="100%" p="5" bg="gray.100">
             <Spacer />
             <Link href="">Home</Link>
+            <Spacer />
             <Link href="">Work</Link>
+            <Spacer />
             <Link href="">About</Link>
+            <Spacer />
             <Link href="">Blog</Link>
+            <Spacer />
             <Link href="">Contact</Link>
         </Flex>
     )
