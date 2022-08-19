b2795098365791b98bf7afbebfb1bd541ab3156a
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 11:38:45 -0700

    build Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,15 @@
 import Link from 'next/link';
+import { Flex, Spacer } from '@chakra-ui/react'
+
+export default function Navbar() {
+    return (
+        <Flex direction="row" w="100%" p="5" bg="gray.100">
+            <Spacer />
+            <Link href="">Home</Link>
+            <Link href="">Work</Link>
+            <Link href="">About</Link>
+            <Link href="">Blog</Link>
+            <Link href="">Contact</Link>
+        </Flex>
+    )
+}
