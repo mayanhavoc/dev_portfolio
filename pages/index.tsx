a32efdde67c362c05d3e73f0421065e861ffbcfa
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 13:09:48 -0700

    mv index to pages dir

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+      Box
+    , Heading
+    , Text
+    , Spinner
+    , Center,
+} from '@chakra-ui/react';
+import { useEffect, useState } from "react";
+import type { NextPage } from "next";
+
+const Home : NextPage = () => {
+    return (
+        <Box>
+            <Heading>Full Stack Developer</Heading>
+        </Box>
+    )
+}
+
+export default Home
