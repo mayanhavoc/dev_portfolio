4af95c4227d41936dc45e7ba4241989890800659
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 11:41:06 -0700

    build app file with ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,17 @@
+import type { AppProps } from 'next/app';
 import { ChakraProvider, Box } from "@chakra-ui/react";
+
+import Navbar from "../components/Navbar";
+
+function MyPortfolio({Component, pageProps}: AppProps){
+    return(
+        <ChakraProvider>
+            <Navbar />
+            <Box w='80%' mx='auto' p='5' mb='24'>
+                <h1>Hello world</h1>
+            </Box>
+        </ChakraProvider>
+    )
+}
+
+export default MyPortfolio
