dfe58e2e5323d15614d5fedd805ffea8145488a8
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-08-19 13:06:35 -0700

    add components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import type { AppProps } from 'next/app';
 import { ChakraProvider, Box } from "@chakra-ui/react";
 
@@ -8,7 +9,7 @@ function MyPortfolio({Component, pageProps}: AppProps){
         <ChakraProvider>
             <Navbar />
             <Box w='80%' mx='auto' p='5' mb='24'>
-                <h1>Hello world</h1>
+                <Component {...pageProps} />
             </Box>
         </ChakraProvider>
     )
